0019d97ffabdf54dccc6736fcda176cb4c34cb1b
illupovero <davideluporini1996@gmail.com>
2022-01-14 15:34:29 +0100

    Reversed word function

---
+++ js/palindromo.js
@@ -0,0 +1,24 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedo all'utente una parola
+let word = prompt('inserisci una parola').trim();
+
+
+// scrivo una funzione per capovolgere la parola
+function reverseWord(word) {
+    let reversedword = '';
+    for (let i = word.lenght - 1; i >= 0; i--) {
+        reversedword += word.charAt(i)
+    }
+    return reverseWord
+
+}
+
+// controllo se la parola inserita e la parola invertita sono uguali
+if (reversedWord === word) {
+
+}
+
+// controllo se la parola inserita e la parola invertita sono uguali
