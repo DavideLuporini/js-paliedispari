6e3d016e51a7daf63031cf8527491101f53d9498
illupovero <davideluporini1996@gmail.com>
2022-01-14 16:46:53 +0100

    added elements to DOM

---
+++ js/numeri.js
@@ -0,0 +1,14 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+let pariODispariValue = document.getElementById('number').value;
+let number = document.getElementById('number');
+
+
+// function parODispari() {
+
+// })
